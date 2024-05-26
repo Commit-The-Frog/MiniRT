135da2e11a41b84bce8aa520d722433393fb59e4
Kirby <maxman306@gmail.com>
2024-05-26 17:23:29 +0900

    feat: work in progress...

--- src/parse/verify.c
+++ src/parse/verify.c
@@ -1,8 +1,14 @@
 #include "minirt.h"
 
+void	exit_with_free(char **list)
+{
+	free_double_char_list(list);
+	exit(1);
+}
+
 // verify one line that seperated with spaces
 // if line is invalid, program exit with freeing every data
 void	verify_line(char **list)
 {
-	
+
 }
