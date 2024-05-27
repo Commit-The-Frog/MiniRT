e5d0c1a41ef8579c301002de0c6104ab338fd34f
Kirby <maxman306@gmail.com>
2024-05-27 15:47:33 +0900

    feat: norm applied in /render

--- src/render/render.c
+++ src/render/render.c
@@ -5,7 +5,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 void	init_mlx(t_info *info)
@@ -13,8 +13,8 @@ void	init_mlx(t_info *info)
 	info->mlx = mlx_init();
 	info->mlx_win = mlx_new_window(info->mlx, WIN_WIDTH, WIN_HEIGHT, "miniRT");
 	info->img.img = mlx_new_image(info->mlx, WIN_WIDTH, WIN_HEIGHT);
-	info->img.addr = mlx_get_data_addr(info->img.img, &info->img.bits_per_pixel, &info->img.line_length,
-								&info->img.endian);
+	info->img.addr = mlx_get_data_addr(info->img.img, \
+	&info->img.bits_per_pixel, &info->img.line_length, &info->img.endian);
 }
 
 void	render_mlx(t_info info)
@@ -27,15 +27,14 @@ void	render_mlx(t_info info)
 
 unsigned long	rgb_to_hex(t_color color)
 {
-	unsigned long hex;
-    int r;
-    int g;
-    int b;
-	
+	unsigned long	hex;
+	int				r;
+	int				g;
+	int				b;
+
 	r = (int)round(color.r);
 	g = (int)round(color.g);
 	b = (int)round(color.b);
-
 	hex = (r << 16) | (g << 8) | b;
-    return (hex);
+	return (hex);
 }
